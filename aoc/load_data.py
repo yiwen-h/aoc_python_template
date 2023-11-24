1da4c5c23c5e92040c626530c5032d3672f91112
Hon YiWen (MLCSU) <YiWen.Hon@mlcsu.nhs.uk>
2023-11-24 14:22:49 +0000

    setup for aoc done

--- aoc/load_data.py
+++ aoc/load_data.py
@@ -5,10 +5,20 @@ import os
 load_dotenv()
 COOKIE = os.environ.get("SESSION")
 
+def check_file_exists(day):
+    filepath = os.path.join('real_data', '{day}.txt')
+    return os.path.isfile(filepath)
+
 def get_data(day):
-    response = requests.get(f'https://adventofcode.com/2021/day/{day}/input',
+    if check_file_exists(day):
+        with open(f'real_data/{day}.txt') as f:
+            raw_data = f.read()
+    else: 
+        response = requests.get(f'https://adventofcode.com/2021/day/{day}/input',
                             cookies={'session': COOKIE})
-    raw_data = response.content.decode('UTF-8')
+        raw_data = response.content.decode('UTF-8')
+        with open(f'real_data/{day}.txt', 'w') as f:
+            f.write(raw_data)
     return raw_data
 
 def get_test_data(day):
--- tests/test_01.py
+++ tests/test_01.py
@@ -3,7 +3,8 @@ from aoc.load_data import get_test_data
 
 
 test_data = get_test_data(1)
-expected_result_a = 7
+expected_result_part_1 = 7
+
 
 def test_day_1_a():
-    assert day_01.part_1(test_data) == expected_result_a
+    assert day_01.part_1(test_data) == expected_result_part_1
